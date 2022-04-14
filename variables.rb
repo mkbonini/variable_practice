eebf1a64a3aebdcdeb7ad8afb785aec009b39676
mkbonini <mkbonini@gmail.com>
2022-04-13 20:18:50 -0600

    add notes to clarify code

--- variables.rb
+++ variables.rb
@@ -1,8 +1,12 @@
+#assigning string variables with lyrics from Rick Astley's never gunna give you up
 first_line = "never gunna give you up"
 second_line = "never gunna let you down"
+#assigning a famous phone number as an integer.  This is probably a
+#dangerous way to save this data
 phone_number = 8675309
-cost_of_dollar = 1.0
+miles_walked = 500.0
 is_rickrolled = true
 
+#reassign string variables using the next lines of the song
 first_line = "never going to run around and desert you"
 second_line = "never make you cry"
