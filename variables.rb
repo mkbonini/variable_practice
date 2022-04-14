a95f8ec296be1a2dc02e8d68c6ba85a3ea135158
mkbonini <mkbonini@gmail.com>
2022-04-13 20:13:37 -0600

    reassign string varaible with new values

--- variables.rb
+++ variables.rb
@@ -3,3 +3,6 @@ second_line = "never gunna let you down"
 phone_number = 8675309
 cost_of_dollar = 1.0
 is_rickrolled = true
+
+first_line = "never going to run around and desert you"
+second_line = "never make you cry"
